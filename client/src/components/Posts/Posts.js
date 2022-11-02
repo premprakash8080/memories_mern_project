48a50d8bf4e068d56fa8239bb400c8f96530872a
premprakash8080 <premprakash8080@gmail.com>
2022-11-02 20:32:58 +0530

    Added styles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Post from './Post/Post';
+import useStyles from './styles';
 
 const Posts = () => {
+    const classes = useStyles();
     return (
+        <>
+
         <h1>POsts</h1>
+        <Post/>
+        <Post/>
+        </>
     );
 }
 
