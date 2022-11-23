e08f234c189689c738069757371445ddebab47ad
premprakash8080 <premprakash8080@gmail.com>
2022-11-24 00:26:10 +0530

    updated

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,35 +7,34 @@ import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
-    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
-    const dispatch = useDispatch();
-    const classes = useStyles();
-  
-    useEffect(() => {
-      if (post) setPostData(post);
-    }, [post]);
-  
-    const clear = () => {
-      setCurrentId(0);
-      setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      if (currentId === 0) {
-        dispatch(createPost(postData));
-        clear();
-      } else {
-        dispatch(updatePost(currentId, postData));
-        clear();
-      }
-    };
-  
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    return (
-        <Paper className={classes.paper}>
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
+  };
+
+  return (
+    <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
@@ -47,7 +46,7 @@ const Form = ({ currentId, setCurrentId }) => {
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
     </Paper>
-    );
-}
+  );
+};
 
 export default Form;
